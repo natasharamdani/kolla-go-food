c05f102f1e31398dd5aa21971cb9dcbbe357b543
Natasha <natasha.aufanada@gmail.com>
2017-10-25 13:18:11 +0700

    factory

---
+++ spec/factories/food.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :food do
+    name "Nasi Uduk"
+    description "Betawi style steamed rice cooked in coconut milk."
+    price 10000.0
+    image_url "image.jpg"
+  end
+end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,52 +1,25 @@
 require 'rails_helper'
 
 describe Food do
-
   it "is valid with a name and description" do
-    food = Food.new(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk.",
-      price: 10000.0
-    )
-    expect(food).to be_valid
+    expect(FactoryGirl.build(:food)).to be_valid
   end
 
-  describe "presence of name and description" do
-    before :each do
-      @food = Food.new(
-      name: nil,
-      description: nil,
-      price: 10000.0
-    )
-    end
+  it "is invalid without a name" do
+    food = FactoryGirl.build(:food, name: nil)
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
 
-    context "without name" do
-      it "is invalid without a name" do
-        @food.valid?
-        expect(@food.errors[:name]).to include("can't be blank")
-      end
-    end
-
-    context "without description" do
-      it "is invalid without a description" do
-        @food.valid?
-        expect(@food.errors[:description]).to include("can't be blank")
-      end
-    end
+  it "is invalid without a description" do
+    food = FactoryGirl.build(:food, description: nil)
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate name" do
-    food1 = Food.create(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk.",
-      price: 10000.0
-    )
-
-    food2 = Food.new(
-      name: "Nasi Uduk",
-      description: "Different description.",
-      price: 10000.0
-    )
+    food1 = FactoryGirl.create(:food, name: "Nasi Uduk")
+    food2 = FactoryGirl.build(:food, name: "Nasi Uduk")
 
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
@@ -87,34 +60,24 @@ describe Food do
   end
 
   it "is invalid with a non numeric price" do
-    food = Food.new(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk.",
-      price: "rupiah"
-    )
+    food = FactoryGirl.build(:food, price: "rupiah")
     food.valid?
     expect(food.errors[:price]).to include("is not a number")
   end
 
   it "is invalid with a price less than 0.01" do
-    food = Food.new(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk.",
-      price: 0.001
-    )
+    food = FactoryGirl.build(:food, price: 0.001)
     food.valid?
     expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
   end
 
   it "is invalid with an image_url other than .gif, .jpg, or .png" do
-    food = Food.new(
-      name: "Nasi Uduk",
-      description: "Betawi style steamed rice cooked in coconut milk.",
-      price: 10000.0,
-      image_url: "image.jpeg"
-    )
+    food = FactoryGirl.build(:food, image_url: "image.jpeg")
     food.valid?
     expect(food.errors[:image_url]).to include("must be an URL for GIF, JPG, or PNG image")
   end
 
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:food)).to be_valid
+  end
 end
