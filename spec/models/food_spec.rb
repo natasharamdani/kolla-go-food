f502bc91738de0268f05f7bf8c4fd287220ecedc
Natasha <natasha.aufanada@gmail.com>
2017-10-25 13:27:13 +0700

    edit rails_helper

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 describe Food do
   it "is valid with a name and description" do
-    expect(FactoryGirl.build(:food)).to be_valid
+    expect(build(:food)).to be_valid
   end
 
   it "is invalid without a name" do
-    food = FactoryGirl.build(:food, name: nil)
+    food = build(:food, name: nil)
     food.valid?
     expect(food.errors[:name]).to include("can't be blank")
   end
 
   it "is invalid without a description" do
-    food = FactoryGirl.build(:food, description: nil)
+    food = build(:food, description: nil)
     food.valid?
     expect(food.errors[:description]).to include("can't be blank")
   end
 
   it "is invalid with a duplicate name" do
-    food1 = FactoryGirl.create(:food, name: "Nasi Uduk")
-    food2 = FactoryGirl.build(:food, name: "Nasi Uduk")
+    food1 = create(:food, name: "Nasi Uduk")
+    food2 = build(:food, name: "Nasi Uduk")
 
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
@@ -60,24 +60,24 @@ describe Food do
   end
 
   it "is invalid with a non numeric price" do
-    food = FactoryGirl.build(:food, price: "rupiah")
+    food = build(:food, price: "rupiah")
     food.valid?
     expect(food.errors[:price]).to include("is not a number")
   end
 
   it "is invalid with a price less than 0.01" do
-    food = FactoryGirl.build(:food, price: 0.001)
+    food = build(:food, price: 0.001)
     food.valid?
     expect(food.errors[:price]).to include("must be greater than or equal to 0.01")
   end
 
   it "is invalid with an image_url other than .gif, .jpg, or .png" do
-    food = FactoryGirl.build(:food, image_url: "image.jpeg")
+    food = build(:food, image_url: "image.jpeg")
     food.valid?
     expect(food.errors[:image_url]).to include("must be an URL for GIF, JPG, or PNG image")
   end
 
   it "has a valid factory" do
-    expect(FactoryGirl.build(:food)).to be_valid
+    expect(build(:food)).to be_valid
   end
 end
