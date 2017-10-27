d7cf1c4db4e9edfc9a5b7254a72c6dc6dc09b617
Natasha <natasha.aufanada@gmail.com>
2017-10-27 09:47:54 +0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   resources :buyers
   resources :foods
-  get 'about/contact'
-
-  get 'home/hello'
-  get 'home/goodbye'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
