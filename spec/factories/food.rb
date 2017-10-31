56a67a8d0a7236932ed64cbaf1e98fb27b131fe9
Natasha <natasha.aufanada@gmail.com>
2017-10-31 11:36:03 +0700

    drinks

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -3,15 +3,11 @@ FactoryGirl.define do
     name { Faker::Food.dish }
     description { Faker::Food.ingredient }
     price 10000.0
+    association :category
   end
 
   factory :invalid_food, parent: :food do
     name nil
     description nil
-    price 10000.0
-  end
-
-  factory :food_category, parent: :food do
-    association :category
   end
 end
