b389399361edb3a05e42ba900e4341931cf9dd25
Natasha <natasha.aufanada@gmail.com>
2017-11-02 16:55:52 +0700

    session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class SessionsController < ApplicationController
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+
   root 'store#index', as: 'store_index'
 
   resources :foods
