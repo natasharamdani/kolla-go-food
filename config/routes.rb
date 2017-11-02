4c61d22ce1c8b21fad020aed817f35c1b654c4a4
Natasha <natasha.aufanada@gmail.com>
2017-11-02 16:22:40 +0700

    session

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :orders
-  resources :categories
-  resources :line_items
-  resources :carts
-  resources :buyers
-  resources :foods
   root 'store#index', as: 'store_index'
+
+  resources :foods
+  resources :buyers
+  resources :carts
+  resources :line_items
+  resources :categories
+  resources :orders
+  resources :users
+
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
