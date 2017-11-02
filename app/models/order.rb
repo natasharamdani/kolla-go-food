d95ed0342105b4f1d3cce6e66dbd67b775916637
Natasha <natasha.aufanada@gmail.com>
2017-11-02 13:16:44 +0700

    order view

--- app/models/order.rb
+++ app/models/order.rb
@@ -20,4 +20,12 @@ class Order < ApplicationRecord
       line_items << item
     end
   end
+
+  def total_price
+    total_price = 0
+    line_items.each do |item|
+      total_price += item.total_price
+    end
+    total_price
+  end
 end
