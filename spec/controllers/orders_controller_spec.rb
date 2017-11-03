d61c83eb356367ea91cb4cfb854e6de02f0fd276
Natasha <natasha.aufanada@gmail.com>
2017-11-03 11:42:32 +0700

    email

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe OrdersController do
+  before :each do
+    user = create(:user)
+    session[:user_id] = user.id
+  end
 
   let(:valid_session) { {} }
 
--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -8,7 +8,7 @@ describe OrderMailer do
     it "sends confirmation email when order is received" do
       expect(mail.subject).to eq("Go Food Order Confirmation")
       expect(mail.to).to eq([order.email])
-      expect(mail.from).to eq([go.scholarship.kolla@mail.com])
+      expect(mail.from).to eq(["go.scholarship.kolla@gmail.com"])
     end
   end
 
@@ -19,7 +19,7 @@ describe OrderMailer do
     it "sends confirmation email when order is shipped" do
       expect(mail.subject).to eq("Go Food Order Shipped")
       expect(mail.to).to eq([order.email])
-      expect(mail.from).to eq([go.scholarship.kolla@mail.com])
+      expect(mail.from).to eq(["go.scholarship.kolla@gmail.com"])
     end
   end
 end
